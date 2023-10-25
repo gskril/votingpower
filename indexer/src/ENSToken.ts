36e1147c44d5e90b8595e853993ba605626d572c
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-10-25 12:20:34 -0400

    Remove unused handler from indexer

--- indexer/src/ENSToken.ts
+++ indexer/src/ENSToken.ts
@@ -1,30 +1,5 @@
 import { ponder } from '@/generated'
 
-// ponder.on('ENSToken:Transfer', async ({ event, context }) => {
-//   const { Account } = context.entities
-//   const { from, to, value } = event.params
-
-//   await Account.upsert({
-//     id: from,
-//     create: {
-//       ensTokens: 0n,
-//     },
-//     update: ({ current }) => ({
-//       ensTokens: current.ensTokens - value,
-//     }),
-//   })
-
-//   await Account.upsert({
-//     id: to,
-//     create: {
-//       ensTokens: value,
-//     },
-//     update: ({ current }) => ({
-//       ensTokens: current.ensTokens + value,
-//     }),
-//   })
-// })
-
 ponder.on('ENSToken:DelegateVotesChanged', async ({ event, context }) => {
   const { Delegate } = context.entities
   const { delegate, newBalance } = event.params
