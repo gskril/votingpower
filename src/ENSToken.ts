688a16a9c51893c0662c5901eaa214e806589a09
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-10-25 11:41:00 -0400

    Handle `DelegateVotesChanged` event

--- src/ENSToken.ts
+++ src/ENSToken.ts
@@ -1,26 +1,41 @@
 import { ponder } from '@/generated'
 
-ponder.on('ENSToken:Transfer', async ({ event, context }) => {
-  const { Account } = context.entities
-  const { from, to, value } = event.params
+// ponder.on('ENSToken:Transfer', async ({ event, context }) => {
+//   const { Account } = context.entities
+//   const { from, to, value } = event.params
 
-  await Account.upsert({
-    id: from,
-    create: {
-      ensTokens: 0n,
-    },
-    update: ({ current }) => ({
-      ensTokens: current.ensTokens - value,
-    }),
-  })
+//   await Account.upsert({
+//     id: from,
+//     create: {
+//       ensTokens: 0n,
+//     },
+//     update: ({ current }) => ({
+//       ensTokens: current.ensTokens - value,
+//     }),
+//   })
 
-  await Account.upsert({
-    id: to,
+//   await Account.upsert({
+//     id: to,
+//     create: {
+//       ensTokens: value,
+//     },
+//     update: ({ current }) => ({
+//       ensTokens: current.ensTokens + value,
+//     }),
+//   })
+// })
+
+ponder.on('ENSToken:DelegateVotesChanged', async ({ event, context }) => {
+  const { Delegate } = context.entities
+  const { delegate, newBalance } = event.params
+
+  await Delegate.upsert({
+    id: delegate,
     create: {
-      ensTokens: value,
+      ensVotingPower: newBalance,
     },
-    update: ({ current }) => ({
-      ensTokens: current.ensTokens + value,
-    }),
+    update: {
+      ensVotingPower: newBalance,
+    },
   })
 })
