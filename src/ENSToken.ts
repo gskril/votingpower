8843f5112fb8e600c8e946ceb6b00c5404b5fecb
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-10-25 10:29:49 -0400

    Refactor `Transfer` handler

--- src/ENSToken.ts
+++ src/ENSToken.ts
@@ -4,46 +4,23 @@ ponder.on('ENSToken:Transfer', async ({ event, context }) => {
   const { Account } = context.entities
   const { from, to, value } = event.params
 
-  const beforeBalances = await Account.findMany({
-    where: {
-      id: {
-        in: [from, to],
-      },
-    },
-  })
-
-  const senderBalanceBefore = beforeBalances.find(
-    (account) => account.id === from
-  )
-  const receiverBalanceBefore = beforeBalances.find(
-    (account) => account.id === to
-  )
-
-  const senderBalance = senderBalanceBefore
-    ? senderBalanceBefore.ensTokens - value
-    : 0n
-
-  const recieverBalance = receiverBalanceBefore
-    ? receiverBalanceBefore.ensTokens + value
-    : value
-
   await Account.upsert({
     id: from,
     create: {
-      ensTokens: senderBalance,
+      ensTokens: 0n,
     },
-    update: {
-      ensTokens: senderBalance,
-    },
+    update: ({ current }) => ({
+      ensTokens: current.ensTokens - value,
+    }),
   })
 
   await Account.upsert({
     id: to,
     create: {
-      ensTokens: recieverBalance,
+      ensTokens: value,
     },
-    update: {
-      ensTokens: recieverBalance,
-    },
+    update: ({ current }) => ({
+      ensTokens: current.ensTokens + value,
+    }),
   })
 })
