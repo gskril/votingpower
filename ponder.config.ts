0f54829078b976ed0ea77acbed54955503124932
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-10-24 22:34:57 -0400

    Create Account entity

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,21 +1,22 @@
-import type { Config } from "@ponder/core";
-import { http } from "viem";
+import type { Config } from '@ponder/core'
+import { http } from 'viem'
 
 export const config: Config = {
   networks: [
     {
-      name: "mainnet",
+      name: 'mainnet',
       chainId: 1,
       transport: http(process.env.PONDER_RPC_URL_1),
+      maxRpcRequestConcurrency: 5,
     },
   ],
   contracts: [
     {
-      name: "ENSToken",
-      network: "mainnet",
-      abi: "./abis/ENSToken.json",
-      address: "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72",
+      name: 'ENSToken',
+      network: 'mainnet',
+      abi: './abis/ENSToken.json',
+      address: '0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72',
       startBlock: 13533418,
     },
   ],
-};
+}
--- src/ENSToken.ts
+++ src/ENSToken.ts
@@ -1,9 +1,49 @@
-import { ponder } from "@/generated";
+import { ponder } from '@/generated'
 
-ponder.on("ENSToken:Approval", async ({ event, context }) => {
-  console.log(event.params);
-});
+ponder.on('ENSToken:Transfer', async ({ event, context }) => {
+  const { Account } = context.entities
+  const { from, to, value } = event.params
 
-ponder.on("ENSToken:Claim", async ({ event, context }) => {
-  console.log(event.params);
-});
+  const beforeBalances = await Account.findMany({
+    where: {
+      id: {
+        in: [from, to],
+      },
+    },
+  })
+
+  const senderBalanceBefore = beforeBalances.find(
+    (account) => account.id === from
+  )
+  const receiverBalanceBefore = beforeBalances.find(
+    (account) => account.id === to
+  )
+
+  const senderBalance = senderBalanceBefore
+    ? senderBalanceBefore.ensTokens - value
+    : 0n
+
+  const recieverBalance = receiverBalanceBefore
+    ? receiverBalanceBefore.ensTokens + value
+    : value
+
+  await Account.upsert({
+    id: from,
+    create: {
+      ensTokens: senderBalance,
+    },
+    update: {
+      ensTokens: senderBalance,
+    },
+  })
+
+  await Account.upsert({
+    id: to,
+    create: {
+      ensTokens: recieverBalance,
+    },
+    update: {
+      ensTokens: recieverBalance,
+    },
+  })
+})
